ad973829bd8a6527a97807e151570d0e6ade74ed
Abdessamad <a.mouhassine@gmail.com>
2015-07-25 08:48:52 +0100

    merge config.js with main.js

--- app/main.js
+++ app/main.js
@@ -22,11 +22,40 @@
  * THE SOFTWARE.
  */
 
-define(function(require, exports, module) {
+require.config({
+    baseUlr: "app",
+    
+    paths: {
+        "underscore":           "../vendor/underscore/underscore",
+        "template":             "../vendor/lodash-template-loader/loader",
+        "jquery":               "../vendor/jquery/dist/jquery",
+        "backbone":             "../vendor/backbone/backbone",
+        "backbone.marionette":  "../vendor/marionette/lib/backbone.marionette",
+        "bootstrap":            "../vendor/bootstrap/dist/js/bootstrap"
+    },
+    
+    map: {
+        '*': {
+            'lodash': 'underscore'
+        }
+    },
+    
+    config: {
+        app: {
+            'locale': 'en',
+            'root': "/"
+        }
+    },
+    
+    shim: {
+        'bootstrap': ['jquery']
+    }
+});
+
+
+// kick off the application
+require(['app'], function(app) {
     'use strict';
     
-    var App = require('app');
-    
-    // start the application with specific options
-    App.start({});
+    app.start();
 });
