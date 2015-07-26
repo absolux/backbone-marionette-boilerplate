c7072bc3f6249dff9abc3e995e8722206fe00ca5
Abdessamad <a.mouhassine@gmail.com>
2015-07-26 19:49:11 +0100

    add app user

--- app/collections/users.js
+++ app/collections/users.js
@@ -25,10 +25,10 @@
 define(function(require, exports, module) {
     'use strict';
     
-    var App = require('app');
     var Backbone = require('backbone');
     
     var Collection = Backbone.Collection.extend({
+        model: require('../models/user'),
         url: 'users'
     });
     
