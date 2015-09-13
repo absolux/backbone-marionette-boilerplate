37479fe7f4f45d9611682e03812cf0980902196b
absolux <a.mouhassine@gmail.com>
2015-09-13 23:39:14 +0100

    upgrade to next major version

--- app/app.js
+++ app/app.js
@@ -1,25 +1,5 @@
-/* 
- * The MIT License
- *
- * Copyright 2015 absolux.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/**
+ * 
  */
 
 define(function(require, exports, module) {
@@ -28,36 +8,23 @@ define(function(require, exports, module) {
     // dependencies
     var Backbone = require('backbone');
     var Marionette = require('backbone.marionette');
-    var Router = require('router');
-    var Layout = require('views/layout');
-    var User = require('models/user');
-    
-    // options names
-    var defaultOptions = ['locale', 'root'];
     
     // Application class
     var Application = Marionette.Application.extend({
         initialize: function() {
+            var defaultOptions = ['locale', 'root'];
+            
             this.mergeOptions(module.config(), defaultOptions);
-            
-            // define app router
-            this.router = new Router();
-            
-            // define app layout
-            this.layout = new Layout();
-            
-            // define app user
-            this.user = new User();
-        },
-        
-        onBeforeStart: function() {
-            this.layout.render();
         },
         
         onStart: function(options) {
-            if (! Backbone.history.started ) {
-                Backbone.history.start({pushState: true, root: this.root});
-            }
+            var that = this;
+            
+            require(['router'], function() {
+                if (! Backbone.history.started ) {
+                    Backbone.history.start({pushState: true, root: that.root});
+                }
+            });
         }
     });
     
--- app/router.js
+++ app/router.js
@@ -1,36 +1,32 @@
-/* 
- * The MIT License
- *
- * Copyright 2015 absolux.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/**
+ * 
  */
 
 define(function(require, exports, module) {
     'use strict';
     
-    var App = require('app');
+    var _ = require('underscore');
     var Marionette = require('backbone.marionette');
     
     var Router = Marionette.AppRouter.extend({
-        appRoutes: {}
+        appRoutes: {
+            
+        },
+        
+        /**
+         * alias of navigate navigate, with trigger option set to true
+         * 
+         * @param {string} fragment
+         * @param {object} options
+         * @returns {void}
+         */
+        redirect: function(fragment, options) {
+            options = _.extend({trigger: true}, options);
+            
+            this.navigate(fragment, options);
+        },
+        
     });
     
-    module.exports = Router;
+    module.exports = new Router;
 });
